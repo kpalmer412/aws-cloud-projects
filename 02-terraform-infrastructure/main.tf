fe37ce967fbd9701f7311fc50e99280fb4a3bbcb
Ken Palmer <kenpalmer412@gmail.com>
2026-09-19 16:42:15 -0400

    feat: implement secure cloud workflow authentication via aws oidc

--- 02-terraform-infrastructure/main.tf
+++ 02-terraform-infrastructure/main.tf
@@ -1,8 +1,8 @@
-terraform {
+echo 'terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.0" # Upgraded to v5 to properly support modern import blocks
+      version = "~> 5.0" 
     }
   }
 }
@@ -11,17 +11,37 @@ provider "aws" {
   region = "us-east-1"
 }
 
-# This instructs Terraform to find your existing server and map it to your code
 import {
   to = aws_instance.example
   id = "i-0074101a2aa5a4c4e"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0a02a779008fa3b99"
-  instance_type = "t2.micro"
+  ami                    = "ami-0a02a779008fa3b99"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > /var/www/html/index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
     Name = "HCLS-Phase1-Ubuntu"
   }
 }
+
+resource "aws_security_group" "instance" {
+  name        = "HCLS-Phase1-Ubuntu"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = "vpc-0ee987ece99c06ebc"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}' > main.tf && ls -la main.tf && terraform init && terraform plan
+
